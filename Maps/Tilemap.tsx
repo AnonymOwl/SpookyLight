87a078b567f95cdbb408dee412b1f5ea33620ecc
Charles Alexander <sejour_a@jormungandr.eu>
2018-02-20 20:22:46 +0100

    Added test map
    Test map, Tileset and tsx

---
+++ Maps/Tilemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tilemap" tilewidth="3" tileheight="3" spacing="1" tilecount="646" columns="34">
+ <image source="Tilemap.png" trans="ff00ff" width="135" height="75"/>
+</tileset>
